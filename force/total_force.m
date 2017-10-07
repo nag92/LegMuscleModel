b7b455d07e3bbd42f4fe21241818bccad0df42a9
nag92 <nagoldfarb@wpi.edu>
2017-10-07 15:26:36 -0400

    forces model created

---
+++ force/total_force.m
@@ -0,0 +1,10 @@
+function [ force ] = total_force( a, fv, fl )
+%TOTAL_FORCE Summary of this function goes here
+%   Detailed explanation goes here
+
+    F_max = 0.45*[ 1850;2370;2190;400;1000;5200;1600;3600;1100];
+    
+    
+
+end
+
