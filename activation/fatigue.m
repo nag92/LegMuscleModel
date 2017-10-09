8fd9278ac6f0512afc5a3a439c3ceb5560023073
nag92 <nagoldfarb@wpi.edu>
2017-10-09 10:02:42 -0400

    added loops to functions to allow them to interate over all the muscles

--- activation/fatigue.m
+++ activation/fatigue.m
@@ -2,11 +2,14 @@ function [ dfit ] = fatigue( fit,a,T,f,fit_min )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
     beta = 0.6;
-    lambda = 1 - beta + beta*(f/100)^2;
-    T_fat = T(1);
-    T_rev = T(2);
-    dfit = ((fit_min - fit)*a*lambda)/T_fat + ((1-fit)*(1-a*lambda))/T_rec;
-    
+    dfit = zeros(1,9);
+    for i = 1:9
+        lambda = 1 - beta + beta*(f(ii)/100)^2;
+        T_fat = T(ii,1);
+        T_rev = T(ii,2);
+        dfit(ii) = ((fit_min(ii) - fit(ii)*a(ii)*lambda)/T_fat + ...
+                ((1-fit(ii))*(1-a(ii)*lambda(ii)))/T_rec;
+    end
     
 end
 
